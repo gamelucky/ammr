262831bee0cea1a81d8fd89c709a3c7eced91bbb
gamelucky <bcs17072009@gmail.com>
2025-03-21 19:40:27 +0700

    Update tin20.py

--- tin20.py
+++ tin20.py
@@ -1,2 +1,7 @@
-s = input()
-print(sum(1 for ch in s if '0' <= ch <= '9'))
+def chuso(s):
+    return sum(1 for ch in s if '0' <= ch <= '9')
+def tanh(s):
+    return sum(1 for ch in s if ('a' <= ch <= 'z') or ('A' <= ch <= 'Z'))
+s = input("Nhập một xâu ký tự: ")
+print("Tổng số ký tự là chữ số:", chuso(s))
+print("Tổng số ký tự là chữ cái tiếng Anh:", tanh(s))
